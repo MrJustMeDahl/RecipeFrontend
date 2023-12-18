4ab5ce14a4695684e15493513172bb139318adb3
Pelle Hald Vedsmand <cph-pv73@cphbusiness.dk>
2023-12-18 22:38:43 +0100

    Design changes and added some more mock data

--- src/components/RecommendedRecipes.jsx
+++ src/components/RecommendedRecipes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { recipes } from '../recipesDB';
-import "../styling/Recommended.css";
+import "../styling/RecipeDetails.css";
 
 const RecommendedRecipes = () => {
 
--- src/recipesDB.js
+++ src/recipesDB.js
@@ -1,26 +1,32 @@
 // recipesDB.js
 export const recipes = [
-    {
-        id: 1,
-        name: 'Spaghetti Carbonara',
-        items: '1 2 3 4 5 6 7 8',
-        description: 'A classic Italian pasta dish with eggs, cheese, pancetta, and black pepper.',
-        instructions: 'you gotta do like this'
-    },
-    {
-        id: 2,
-        name: 'Chicken Tikka Masala',
-        items: '1 2 3 4 5 6 7 8',
-        description: 'A popular Indian curry dish with roasted chicken chunks in a spiced creamy sauce.',
-        instructions: 'you gotta do like this'
-
-    },
-    {
-        id: 3,
-        name: 'Vegetable Stir Fry',
-        items: '1 2 3 4 5 6 7 8',
-        description: 'A quick and easy stir-fry with mixed vegetables and your choice of sauce.',
-        instructions: 'you gotta do like this'
-    }
-    // Add more recipes as needed
+  {
+    id: 1,
+    name: "Spaghetti Carbonara",
+    items:
+      "250g spaghetti, 100g bacon, 2 æg, 50g parmesan, salt, peber, 1 løg i tern, 1 tsk smør, 1 dl piskefløde",
+    description:
+      "A classic Italian pasta dish with eggs, cheese, pancetta, and black pepper.",
+    instructions:
+      "Kog pastaen, til den er perfekt mør med bid. Steg bacon på en pande ved middelhøj varme, til det er sprødt og stadig saftigt. Kom bacon på en tallerken med fedtsugende papir. Kom smør på panden med baconfedt og sauter løgene, til er bløde og klare. Tilsæt bacon og fløde. Lad fløden bruse kort op. Tag panden af varmen og rør æg i samt den kogte pasta, og bland retten godt sammen med den friskrevne ost. Smag til med salt og friskkværnet peber.",
+  },
+  {
+    id: 2,
+    name: "Chicken Tikka Masala",
+    items:
+      "2 dl græsk yoghurt 10 %, 4 fed hvidløg finthakket, 3 tsk ingefær, 3 tsk garam masala, 1.50 tsk stødt gurkemeje, 1 tsk chiliflager, 0,50 tsk stødt nellike, 2 lime, saften herfra, salt og peber, 600 g kyllingebryst eller inderfilet, 2 løg, finthakket",
+    description:
+      "A popular Indian curry dish with roasted chicken chunks in a spiced creamy sauce.",
+    instructions:
+      "Sauter løgene i en gryde med olie ved middelvarme under omrøring, til de er blanke og gennemsigtige. Tilsæt hvidløg, ingefær, gurkemeje, mandler, tomatpuré, og vand.",
+  },
+  {
+    id: 3,
+    name: "Vegetable Stir Fry",
+    items: "1 2 3 4 5 6 7 8",
+    description:
+      "A quick and easy stir-fry with mixed vegetables and your choice of sauce.",
+    instructions: "you gotta do like this",
+  },
+  // Add more recipes as needed
 ];
